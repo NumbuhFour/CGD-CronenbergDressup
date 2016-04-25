47931e571adf5c6436baadb52d3e87940d6a3673
unknown <jamie44344@gmail.com>
2016-04-25 15:43:20 -0400

    made a working main menu

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour {
+    //Different menu pages
+    public GameObject main;
+    public GameObject howTo;
+    public GameObject options;
+    //Methods for changing scenes
+    public void StartB()
+    {
+        SceneManager.LoadScene(1);
+        SceneManager.UnloadScene(0);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    //Methods for opening/closing new menus
+    public void Main()
+    {
+        main.SetActive(true);
+        howTo.SetActive(false);
+        options.SetActive(false);
+    }
+    public void HowTo()
+    {
+        main.SetActive(false);
+        howTo.SetActive(true);
+        options.SetActive(false);
+    }
+    public void Options()
+    {
+        main.SetActive(false);
+        howTo.SetActive(false);
+        options.SetActive(true);
+    }
+}
