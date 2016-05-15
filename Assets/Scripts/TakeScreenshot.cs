e9716dc667ee086609a3f32615d727296711f3e8
Jamie Lamoureux <jamie44344@gmail.com>
2016-05-15 13:59:49 -0400

    Screenshot script added

---
+++ Assets/Scripts/TakeScreenshot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeScreenshot : MonoBehaviour {
+
+    public GameObject menu;
+    string path;
+
+    void Start()
+    {
+        path = Application.persistentDataPath;
+    }
+    public void ScreenShot()
+    {
+        menu.SetActive(false);
+        //take screenshot
+        Application.CaptureScreenshot(path + "/" + "Screenshot.png");
+        menu.SetActive(true);
+    }
+}
